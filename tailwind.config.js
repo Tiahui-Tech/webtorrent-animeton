ca9c63c1377a7de4f9289f32988a1db62079a3c8
Rafael Ayala <asuregames01@gmail.com>
2024-07-26 17:52:44 -0600

    Styles fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   content: [
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./static/*.{css}"
   ],
   theme: {
     extend: {},
