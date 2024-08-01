ad7cb656ad755b285dc24d41d2841cf2b6402484
Rafael Ayala <asuregames01@gmail.com>
2024-07-31 19:28:23 -0600

    Removed unused code

--- src/renderer/pages/torrent-list-page.js
+++ src/renderer/pages/torrent-list-page.js
@@ -1,15 +1,11 @@
 const React = require('react')
-// const { dispatcher } = require('../lib/dispatcher')
 
 const RssFeed = require('../components/torrent-list/rss-feed');
 const AnimeFeed = require('../components/torrent-list/anime-feed');
 
 const TorrentList = ({ state }) => {
   return (
-    <div
-      // key='torrent-list'
-      // onContextMenu={dispatcher('openTorrentListContextMenu')}
-    >
+    <div>
       <RssFeed state={state} />
       <AnimeFeed state={state} />
     </div>
