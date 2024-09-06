6c5ea680710e77378536f1eba974f3fc97e61e85
Rafael Ayala <asuregames01@gmail.com>
2024-09-06 17:09:39 -0600

    Subtitles worker

--- src/modules/subtitles-worker.js
+++ src/modules/subtitles-worker.js
@@ -1,15 +1,3 @@
-// const { parentPort } = require('worker_threads');
-// const { parseSubtitles } = require('./subtitles-parser');
-
-// parentPort.on('message', async (filePath) => {
-//   try {
-//     const subtitles = await parseSubtitles(filePath);
-//     parentPort.postMessage({ success: true, subtitles });
-//   } catch (error) {
-//     parentPort.postMessage({ success: false, error: error.message });
-//   }
-// });
-
 const { parseSubtitles } = require('./subtitles-parser')
 
 process.on('message', async (filePath) => {
