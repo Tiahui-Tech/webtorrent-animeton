62528b2dc3851c66cf6f3fb849e1f1c71b4305ba
Rafael Ayala <asuregames01@gmail.com>
2024-10-03 13:39:39 -0600

    removed magicui and fixed package.js

--- src/renderer/components/anime/AnimeCarousel/index.js
+++ src/renderer/components/anime/AnimeCarousel/index.js
@@ -6,7 +6,6 @@ const { motion, AnimatePresence } = require('framer-motion');
 const { translateGenres } = require('../../../../modules/utils');
 
 const { Icon } = require('@iconify/react');
-const FadeText = require('../../ui/MagicUI/Effects/FadeText');
 
 const AnimeCarousel = ({ animes }) => {
     const navigate = useNavigate();
@@ -78,14 +77,14 @@ const AnimeCarousel = ({ animes }) => {
                         <div className="relative h-full flex flex-row justify-center items-center gap-8 text-white py-12">
                             <div className="flex flex-col justify-between max-w-[60%] h-full">
                                 <div className="flex flex-col gap-2">
-                                    <FadeText
-                                        direction="left"
-                                        framerProps={{
-                                            show: { transition: { delay: 0.3 } },
-                                        }}
+                                    <motion.p
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        transition={{ delay: 0.6, duration: 0.5 }}
                                         className="text-5xl font-bold text-left w-full"
-                                        text={currentAnime.title.romaji}
-                                    />
+                                    >
+                                        {currentAnime.title.romaji}
+                                    </motion.p>
                                     <motion.div
                                         initial={{ opacity: 0, x: -50 }}
                                         animate={{ opacity: 1, x: 0 }}
